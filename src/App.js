510ca9b5d545cae0c59151927ff72027d166c834
Tharindu Bandara <tharindu.bandara@madmobile.com>
2024-08-20 08:35:00 +0530

    test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className='App-logo' alt='logo' />
         <div style={{ fontSize: 30 }}>
           Version{' '}
-          <span style={{ color: 'red' }}>third Commit.........!!!</span>
+          <span style={{ color: 'red' }}>fourth Commit.........!!!</span>
         </div>
       </header>
     </div>
