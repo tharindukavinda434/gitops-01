221516045ecd5f447cad6c8fa0c109d501b819d6
Tharindu Bandara <tharindu.bandara@madmobile.com>
2024-08-29 11:36:52 +0530

    tenth commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className='App-logo' alt='logo' />
         <div style={{ fontSize: 30 }}>
           Version{' '}
-          <span style={{ color: 'red' }}>nineth Commit.........!!!</span>
+          <span style={{ color: 'red' }}>tenth Commit.........!!!</span>
         </div>
       </header>
     </div>
